9fa6a440f8b9146c7a10209bc7a371ff7810f7ce
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-05-28 13:01:16 +0300

    complete array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers , sam) {
+    
+    return drivers.filter(driver => driver.toLowerCase() === sam.toLowerCase());
+
+}
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+// Function to check if a name matches in the data structure
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+
+
+
+
